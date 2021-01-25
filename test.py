b50a67b21afc9dfdb835f8b56a078b9b2e5b0002
nttuan8 <thanhtuanvp88@gmail.com>
2021-01-25 22:34:10 +0700

    update gitingore

--- test.py
+++ test.py
@@ -3,22 +3,16 @@ from utils.capture import VideoCaptureThreading
 import time
 
 if __name__=='__main__':
-    url_http = 'http://admin:dslabneu8@192.168.0.103:80/ISAPI/streaming/channels/102/httppreview'
-    cap = VideoCaptureThreading(url_http)
-    cap.start()
-
+    camera = cv2.VideoCapture('http://admin:dslabneu8@192.168.0.103:80/ISAPI/streaming/channels/102/httppreview')
     t0 = time.time()
     i = 0
     while True:
-        _, frame = cap.read()
+        _, frame = camera.read()
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         i += 1
-
-    n_frames = i
-    print('Frames per second: {:.2f}, with_threading={}'.format(n_frames / (time.time() - t0), True))
-    print("Num_frames: {}".format(n_frames))
-    print("total time: {:.2f}".format(time.time() - t0))
-    cap.stop()
-    cv2.destroyAllWindows()
+        if time.time() - t0 > 1:
+            print(i)
+            t0 = time.time()
+            i = 0

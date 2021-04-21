f7466dead1f428e0f6ec2dbe9ea489aab7bf6bbe
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2021-04-21 23:38:01 +0700

    change run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from face_recognition import main
-
+import asyncio
 
 if __name__ == '__main__':
-    main(tensorrt=True, cam_device=None, input_size=(480, 640))
+    futures = [main(tensorrt=True, cam_device=None, input_size=(480, 640))]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(futures))
